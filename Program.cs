aba693ede9132555dce39a05cdf50c4062f91a2f
1SurajDesai <surajdesai219@gmail.com>
2022-03-02 13:05:41 +0530

    Adding Program.cs file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            
+            //CreateContactsInAddressBook Person = new CreateContactsInAddressBook();
+            //Person.AddPerson();
+        }
+    }
+}
